6bac1c03a7aff3de892773ec6b42b336001adbb9
Kristof Keppens <kkeppens@gmail.com>
2015-08-12 12:54:08 +0200

    fixes #4, styling footer with contact info and other details

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,21 +1,35 @@
 <footer>
-	<div class="container">
-		<p>Contact: <b>ICTO</b></p>
-		<ul class="language-bar-switcher">
-			<li><i class="fa fa-language fa-2x"></i></li>
-			@foreach(LaravelLocalization::getSupportedLocales() as $localeCode => $properties)
-				<li>
-					<a rel="alternate" hreflang="{{$localeCode}}" href="{{LaravelLocalization::getLocalizedURL($localeCode) }}">
-						{{{ $properties['native'] }}}
-					</a>
-				</li>
-			@endforeach
-		</ul>
-		<address>
-			<ul class="company-social">
-				<li class="social-facebook"><a href="https://www.facebook.com/minerva.ugent" target="_blank"><i class="fa fa-facebook"></i></a></li>
-				<li class="social-github"><a href="https://github.com/ICTO/BBB-webmeetings" target="_blank"><i class="fa fa-github"></i></a></li>
+	<div class="container-fluid">
+		<div class="col-md-6">
+			<ul class="language-bar-switcher">
+				<li><i class="fa fa-language fa-2x"></i></li>
+				@foreach(LaravelLocalization::getSupportedLocales() as $localeCode => $properties)
+					<li>
+						<a rel="alternate" hreflang="{{$localeCode}}" href="{{LaravelLocalization::getLocalizedURL($localeCode) }}">
+							{{{ $properties['native'] }}}
+						</a>
+					</li>
+				@endforeach
 			</ul>
-		</address>
+			<div class="contact">
+				Contact: <b><a href="mailto:icto@ugent.be?Subject=Webmeeting">Icto</a></b>
+			</div>
+		</div>
+		<div class="col-md-6">
+			<address>
+				<ul class="company-social">
+					<li class="social-facebook">
+						<a href="https://www.facebook.com/minerva.ugent" target="_blank">
+								<i class="fa fa-facebook fa-4x"></i>
+						</a>
+					</li>
+					<li class="social-github">
+						<a href="https://github.com/ICTO/BBB-webmeetings" target="_blank">
+							<i class="fa fa-github fa-4x"></i>
+						</a>
+					</li>
+				</ul>
+			</address>
+		</div>
 	</div>
 </footer>
