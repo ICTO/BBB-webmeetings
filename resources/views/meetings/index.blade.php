030414b63a237e9bbca00f0724651961bae34ebb
Kristof Keppens <kkeppens@gmail.com>
2015-08-14 16:17:24 +0200

    fix #27, localization of link to telefoonboek

--- resources/views/meetings/index.blade.php
+++ resources/views/meetings/index.blade.php
@@ -9,7 +9,7 @@
         <meeting v-repeat="meetings | filterBy search | paginate">
             <h2><a href="/meeting/@{{ id }}">@{{ title}}</a></h2>
             <div class="clearfix">
-                <div class="creator pull-left small">{{ trans('meetings.creator') }} <a href="http://telefoonboek.ugent.be/simple?name=@{{ user.full_name }}">@{{ user.full_name }}</a></div>
+                <div class="creator pull-left small">{{ trans('meetings.creator') }} <a href="http://telefoonboek.ugent.be/{{ LaravelLocalization::getCurrentLocale() }}/simple?name=@{{ user.full_name }}">@{{ user.full_name }}</a></div>
                 <div class="created_at pull-right small">@{{ updated_at }}</div>
             </div>
             <div class="body lead">@{{ description }}</div>
